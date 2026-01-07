5813c4f55df17a97fbe7c67847adf90f1cfc4001
Adnan <adnanmuhammedadnancp@gmail.com>
2026-01-07 20:55:31 +0530

    fix: subshell global variable scope issue

--- bluetooth.sh
+++ bluetooth.sh
@@ -16,6 +16,10 @@
 
 declare -A DEVICES # key(name) : value(mac addr)
 
+log_info() {
+	echo "[INFO]: $*" >&2
+}
+
 is_bt_on() {
 	bluetoothctl show | awk -F': ' '/Powered/ {print $2}' | grep -q "yes"
 }
@@ -28,30 +32,53 @@ toggle_bt_power() {
 	fi
 }
 
-pair_device() {
-	local device_mac=$1
-	$(bluetoothctl pair "$device_mac")
+is_device_connected() {
+	bluetoothctl info $1 | awk -F': ' '/Connected/ {print $2}'
+}
+
+connect_bt_device() {
+	bluetoothctl connect $1
+}
+
+disconnect_bt_device() {
+	bluetoothctl disconnect $1
+}
+
+forget_bt_device() {
+        bluetoothctl remove $1
 }
 
 toggle_device_connection() {
 	local device=$1
-	local is_connected=$(bluetoothctl info $1 | awk -F': ' '/Connected/ {print $2}')
-	if [ "$is_connected" = "yes" ]; then
-		echo "Device $1 already connected, Disconnecting..."
-		bluetoothctl disconnect $1
+	if [ "$(is_device_connected $device)" = "yes" ]; then
+		echo "Device $device already connected, Disconnecting..."
+		disconnect_bt_device "$device"
 	else
-		echo "Device $1 not connected, connecting..."
-		bluetoothctl connect $1
+		echo "Device $device not connected, connecting..."
+		connect_bt_device "$device"
 	fi
 }
 
 list_devices() {
+	log_info "list_devices() called"
+
 	mapfile -t paired_device_macs < <(
 		bluetoothctl devices Paired
 	)
 
+	log_info "Raw paired devices:"
+	for d in "${paired_device_macs[@]}"; do
+		log_info "  $d"
+	done
+
+	DEVICES=()
+
 	for device in "${paired_device_macs[@]}"; do
 		local device_name=$(echo "$device" | awk -F' ' '{$1=$2="";sub(/^ */, "");print}')
-		DEVICES[$device_name]=$(echo "$device" | awk -F' ' '{print $2}')
+		local mac=$(echo "$device" | awk -F' ' '{print $2}')
+		DEVICES["$device_name"]="$mac"
+		log_info "Mapped: [$device_name] -> [$mac]"
 	done
+
+	log_info "Final device keys: ${!DEVICES[*]}"
 }
