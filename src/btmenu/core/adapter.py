1a8813edfd9bc26e4c70d736a8234f833de40c43
Adnan <adnanmuhammedadnancp@gmail.com>
2026-05-28 22:58:15 +0530

    add needed information from adapter

--- src/btmenu/core/adapter.py
+++ src/btmenu/core/adapter.py
@@ -1,15 +1,33 @@
+import json
 from btmenu.core.base import BtMenuBaseDBusClient
 
 
 class BtMenuAdapterClient(BtMenuBaseDBusClient):
-    async def get_adapter_details(self) -> dict:
+    async def get_adapter_details(self) -> str:
         proxy = await self.get_proxy()
 
         adapter = proxy.get_interface(
             "org.freedesktop.DBus.Properties",
         )
 
-        device_name = await adapter.call_get("org.bluez.Adapter1", "Address")
-        return {
-            "result": device_name.value,
-        }
+        values = (
+            "Address",
+            "Name",
+            "Alias",
+            "Pairable",
+            "Powered",
+            "Discoverable",
+            "Connectable",
+            "Version",
+            "Discovering",
+        )
+
+        device_info = {}
+
+        for each in values:
+            value = await adapter.call_get("org.bluez.Adapter1", each)
+            device_info[each.lower()] = value.value
+
+        json_str = json.dumps({device_info["name"]: device_info}, indent=4)
+
+        return json_str
