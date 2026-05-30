e654d26b8830014cbce1c95fac6241f04946242b
Adnan <adnanmuhammedadnancp@gmail.com>
2026-05-30 23:32:22 +0530

    add option to power the adapter

--- src/btmenu/core/adapter.py
+++ src/btmenu/core/adapter.py
@@ -1,5 +1,7 @@
 import json
+from typing import Literal
 from btmenu.core.base import BtMenuBaseDBusClient
+from dbus_next import Variant
 
 
 class BtMenuAdapterClient(BtMenuBaseDBusClient):
@@ -31,3 +33,17 @@ class BtMenuAdapterClient(BtMenuBaseDBusClient):
         json_str = json.dumps({device_info["name"]: device_info}, indent=4)
 
         return json_str
+
+    async def toggle_power(self, state: Literal["on", "off"]):
+        proxy = await self.get_proxy()
+
+        adapter = proxy.get_interface(
+            "org.freedesktop.DBus.Properties",
+        )
+
+        await adapter.call_set(
+            "org.bluez.Adapter1",
+            "Powered",
+            Variant("b", True if state == "on" else False),
+        )
+        return state
